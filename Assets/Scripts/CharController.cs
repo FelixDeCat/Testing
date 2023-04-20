2da22206d1b2154b6b962589800460cf71fee6d4
Guillermo Felix Romero <guillermofelixromero@gmail.com>
2023-04-20 12:25:04 -0300

    le agrego funcionalidad al controller

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Inputs
+{
+    axis,
+    touchs,
+    other
+}
 public class CharController : MonoBehaviour
 {
     Vector3 v3Move;
+    bool touch = false;
+    Inputs input = Inputs.axis;
     public Vector3 Move
     {
         get
         {
-            v3Move.x = Input.GetAxis("Horizontal");
-            v3Move.z = Input.GetAxis("Vertical");
+            switch (input)
+            {
+                case Inputs.axis:
+                    v3Move.x = Input.GetAxis("Horizontal");
+                    v3Move.z = Input.GetAxis("Vertical");
+                    break;
+                case Inputs.touchs:
+                    //tomo inputs de eventos de pantalla UI
+                    break;
+                case Inputs.other:
+                    //tengo algun input alienígena
+                    break;
+                default:
+                    //¿porque? no hay porque..
+                    break;
+            }
+
             return v3Move;
         }
     }
